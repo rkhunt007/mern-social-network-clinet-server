db6d95283479eafa12bee9c8f2063c3fb29c486f
Rahul Khunt <rkhunt007@gmail.com>
2018-06-18 23:51:41 +0530

    creating register route

--- models/User.js
+++ models/User.js
@@ -23,6 +23,6 @@ const UserSchema = new Schema({
   }
 });
 
-const User = new mongoose.model("users", UserSchema);
+const User = mongoose.model("users", UserSchema);
 
 module.exports = User;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
